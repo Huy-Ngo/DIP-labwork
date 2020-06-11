c993de4cd4a82b5cb2d2415038b2e1fad033e675
Huy-Ngo <duchuy29092000@gmail.com>
2020-06-11 17:35:53 +0700

    Improve Canny edge detector

--- labwork_2/ex2.py
+++ labwork_2/ex2.py
@@ -1,21 +1,29 @@
-from cv2 import imread, cvtColor, COLOR_BGR2RGB, Canny, Laplacian, Sobel, CV_32F
+from cv2 import imread, cvtColor, COLOR_BGR2RGB, Canny, Laplacian, Sobel, CV_8U, medianBlur, GaussianBlur, blur
 from numpy import array
 import matplotlib.pyplot as plt
 
-img = imread('../resource/target.png')
+img = imread('../resource/inception.jpg')
 img_rgb = cvtColor(img, COLOR_BGR2RGB)
+# img_rgb_blur = blur(img_rgb, (3, 3))
+img_rgb_blur = GaussianBlur(img_rgb, (3, 3), 8)  # Gaussian
+# img_rgb_blur = medianBlur(img_rgb, 3)  # median
 
-img_can = Canny(img_rgb, 10, 100)
-img_lap = Laplacian(img_rgb, CV_32F)
-img_sob = Sobel(img_rgb, CV_32F, 1, 1)
+img_can = Canny(img_rgb, 180, 220, apertureSize=3)
+img_can_blur = Canny(img_rgb_blur, 180, 220, apertureSize=3)
+img_lap = Laplacian(img_rgb, CV_8U)
+img_sob = Sobel(img_rgb, CV_8U, 1, 1)
 
-plt.subplot(221)
+plt.subplot(231)
 plt.title('Original image')
 plt.imshow(img_rgb)
 
-plt.subplot(222)
+plt.subplot(232)
 plt.title('Canny')
 plt.imshow(img_can)
+
+plt.subplot(233)
+plt.title('Canny (with blur)')
+plt.imshow(img_can_blur)
 
 plt.subplot(223)
 plt.title('Laplacian')
--- labwork_2/ex4.py
+++ labwork_2/ex4.py
@@ -1,4 +1,4 @@
-from cv2 import imread, cvtColor, COLOR_BGR2RGB, HoughLines, Canny
+from cv2 import imread, cvtColor, COLOR_BGR2GRAY, HoughLines, Canny, GaussianBlur
 import matplotlib.pyplot as plt
 from numpy import pi, cos, sin
 import matplotlib.lines as mlines
@@ -6,30 +6,32 @@ import matplotlib.lines as mlines
 def newline(p1, p2):
     ax = plt.gca()
     xmin, xmax = ax.get_xbound()
+    ymin, ymax = ax.get_ybound()
 
     if(p2[0] == p1[0]):
         xmin = xmax = p1[0]
-        ymin, ymax = ax.get_ybound()
     else:
         ymax = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmax-p1[0])
         ymin = p1[1]+(p2[1]-p1[1])/(p2[0]-p1[0])*(xmin-p1[0])
+    print(f'({xmin}, {ymin}) -- ({xmax}, {ymax})')
 
     l = mlines.Line2D([xmin,xmax], [ymin,ymax], color='red')
     ax.add_line(l)
     return l
 
 img = imread('../resource/inception.jpg')
-img_rgb = cvtColor(img, COLOR_BGR2RGB)
+img_gray = cvtColor(img, COLOR_BGR2GRAY)
+img_gray = GaussianBlur(img_gray, (3, 3), 4)
 
-edges = Canny(img_rgb, 180, 220, apertureSize=3)
+edges = Canny(img_gray, 180, 220, apertureSize=3)
 
-lines = HoughLines(edges, 1, pi/180, 235)
+lines = HoughLines(edges, 1, pi/180, 150, 100, 10)
 
 plt.subplot(121)
 plt.imshow(edges)
 
 plt.subplot(122)
-plt.imshow(img_rgb)
+plt.imshow(img_gray)
 # print(lines)
 for line in lines:
     for rho, theta in line:
